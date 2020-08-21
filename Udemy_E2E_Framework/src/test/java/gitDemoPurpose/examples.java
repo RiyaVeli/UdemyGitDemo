37383ab527ecebcac3a70b3f7de7fed3ea73a475
RiyaVeli <riya.veli@gmail.com>
2020-08-21 16:32:40 -0400

    Commit from Second Person

---
+++ Udemy_E2E_Framework/src/test/java/gitDemoPurpose/examples.java
@@ -0,0 +1,8 @@
+package gitDemoPurpose;
+
+public class examples {
+	public static void main(String[] args) {
+		System.out.println("just testig ...");
+	}
+
+}
---
+++ Udemy_E2E_Framework/src/test/java/gitDemoPurpose/secondEXAMPLES.java
@@ -0,0 +1,10 @@
+package gitDemoPurpose;
+
+public class secondEXAMPLES {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Keep testing...");
+	}
+
+}
