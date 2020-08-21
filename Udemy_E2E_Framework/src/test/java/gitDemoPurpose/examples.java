9c7ea68c5b9a68ef59a58662e0cf4cc7f9148e51
RiyaVeli <riya.veli@gmail.com>
2020-08-21 17:38:15 -0400

    Architect American Development

--- Udemy_E2E_Framework/src/test/java/gitDemoPurpose/examples.java
+++ Udemy_E2E_Framework/src/test/java/gitDemoPurpose/examples.java
@@ -3,6 +3,9 @@ package gitDemoPurpose;
 public class examples {
 	public static void main(String[] args) {
 		System.out.println("just testig ...");
+		
+		System.out.println("2just testig ...");
+		System.out.println("3just testig ...");
 	}
 
 }
--- Udemy_E2E_Framework/src/test/java/gitDemoPurpose/secondEXAMPLES.java
+++ Udemy_E2E_Framework/src/test/java/gitDemoPurpose/secondEXAMPLES.java
@@ -5,6 +5,9 @@ public class secondEXAMPLES {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Keep testing...");
+		
+		System.out.println("testing2...");
+		System.out.println("testing3...");
 	}
 
 }
